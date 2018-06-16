1381476f6587ebb47297dd8d0fbdf72108fbb2e6
Gerhard Wagner <gerhard@applike.info>
2018-06-16 12:56:34 +0200

    fixed data

--- goapi.go
+++ goapi.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"GoAPI/sensors"
+	"GoAPI/io"
 )
 
 func main() {
@@ -15,16 +16,26 @@ func main() {
 		})
 	})
 
+	//PEW PEW
 	r.PUT("/pew", sensors.LazorHandler(10).Pew(true))
 	r.DELETE("/pew", sensors.LazorHandler(10).Pew(false))
 
+	//IO
+	r.PUT("/io/:pin", io.IoHandler().IoHigh())
+	r.DELETE("/io/:pin", io.IoHandler().IoLow())
+
+	//TEMPHU
 	r.GET("/temp", sensors.TempHuHandler(9).GetTempHu())
 
+	//TEMPDATA
 	r.GET("/temps", func(c *gin.Context) {
-		c.JSON(200, [][]int{ {1, 2}, {3, 5}, {5, 13}, {1, 42}, {3, 45}, {5,53} })
+		c.JSON(200, [][][]int{ { {1, 2}, {3, 5}, {5, 13}, {1, 42}, {3, 45}, {5,53} } })
 	})
+
+	//API REPORT
 	r.GET("/api", func(c *gin.Context) {
 		c.JSON(200, gin.Default().Routes())
 	})
+
 	r.Run(":8888") // listen and serve on 0.0.0.0:8080
 }
--- io/io.go
+++ io/io.go
@@ -4,7 +4,6 @@ import (
 	"github.com/stianeikeland/go-rpio"
 	"github.com/gin-gonic/gin"
 	"strconv"
-	"log"
 )
 
 type ioHandler struct {
@@ -15,7 +14,8 @@ func (i ioHandler) IoHigh() gin.HandlerFunc {
 	if !i.isIoWorking {
 		return func(c *gin.Context) {
 			c.JSON(500,  gin.H{
-				"message": "gpio is not working",
+				"message": "error",
+				"error": "gpio is not working",
 			})
 		}
 	}
@@ -24,12 +24,49 @@ func (i ioHandler) IoHigh() gin.HandlerFunc {
 		pinNum, err := strconv.Atoi(c.Param("pin"))
 
 		if err != nil {
-			log.Fatal("invalid param")
+			c.JSON(500, gin.H{
+				"message": "error",
+				"error": err.Error(),
+			})
 		}
 
 		pin := rpio.Pin(pinNum)
 		pin.Output()
 		pin.High()
+		c.JSON(200, gin.H{
+			"message": "success",
+			"pin": pinNum,
+		})
+	}
+}
+
+func (i ioHandler) IoLow() gin.HandlerFunc {
+	if !i.isIoWorking {
+		return func(c *gin.Context) {
+			c.JSON(500,  gin.H{
+				"message": "error",
+				"error": "gpio is not working",
+			})
+		}
+	}
+
+	return func(c *gin.Context) {
+		pinNum, err := strconv.Atoi(c.Param("pin"))
+
+		if err != nil {
+			c.JSON(500, gin.H{
+				"message": "error",
+				"error": err.Error(),
+			})
+		}
+
+		pin := rpio.Pin(pinNum)
+		pin.Output()
+		pin.Low()
+		c.JSON(200, gin.H{
+			"message": "success",
+			"pin": pinNum,
+		})
 	}
 }
 
