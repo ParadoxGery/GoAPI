6e5427264ba00620a0f3415b6fd3343d58ca5fd7
laflow <lasse@go4more.de>
2018-06-16 13:10:26 +0200

    new graph

--- web/graph.js
+++ web/graph.js
@@ -1,43 +1,37 @@
-$(document).ready(function(){
-    // Our ajax data renderer which here retrieves a text file.
-    // it could contact any source and pull data, however.
-    // The options argument isn't used in this renderer.
-    var ajaxDataRenderer = function(url, plot, options) {
-        var ret = null;
-        $.ajax({
-            // have to use synchronous here, else the function
-            // will return before the data is fetched
-            async: false,
-            url: url,
-            dataType:"json",
-            success: function(data) {
-                ret = data;
-            }
-        });
-        return ret;
-    };
+var plot2 = null;
 
-    // The url for our json data
-    var jsonurl = "jsondata.txt";
-
-    // passing in the url string as the jqPlot data argument is a handy
-    // shortcut for our renderer.  You could also have used the
-    // "dataRendererOptions" option to pass in the url.
-    var plot2 = $.jqplot('chart1',jsonurl,{
-        title: "17. Februar 2018",
-        axes: {
-            xaxis: {
-                label: 'Stunde',
-                labelRenderer: $.jqplot.CanvasAxisLabelRenderer
-            },
-            yaxis: {
-                label: 'Temperatur [°C]',
-                labelRenderer: $.jqplot.CanvasAxisLabelRenderer
-            }
-        },
-        dataRenderer: ajaxDataRenderer,
-        dataRendererOptions: {
-            unusedOptionalUrl: jsonurl
+function fetchAjaxData(url, success) {
+    $.ajax({
+        url: url,
+        dataType:"json",
+        success: function(data) {
+            success(data);
+            console.log('loaded');
         }
     });
+}
+
+function createPlot(url) {
+    fetchAjaxData(url, function(data) {
+        if (plot2 == null) {
+            plot2 = $.jqplot('chart2', data, {
+                title: "AJAX JSON Data Renderer"
+            });
+        } else {
+            plot2.replot({data: data});
+            console.log('replotting');
+        }
+    });
+}
+
+$(document).ready(function(){
+    var jsonurl = "./jsondata.txt";
+
+    //Regenerate the plot on button click.
+    $('#ajax-button').click(function() {
+        createPlot(jsonurl);
+    });
+
+    //Generate the plot first time through.
+    createPlot(jsonurl);
 });
